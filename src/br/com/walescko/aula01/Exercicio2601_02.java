7c24187625b81d04e6f918c5dfa093c7a1f84e52
walescko <walescko@gmail.com>
2022-01-27 10:06:24 -0300

    java - exercicios 2 e 3 - aula 26/01/2022

---
+++ src/br/com/walescko/aula01/Exercicio2601_02.java
@@ -0,0 +1,35 @@
+//As maçãs custam R$ 0,30 cada se forem compradas menos
+//do que uma dúzia, e R$ 0,25 se forem compradas pelo menos doze.
+//Escreva um programa que leia o número de maçãs compradas,
+//calcule e escreva o valor total da compra.
+package br.com.walescko.aula01;
+
+import java.util.Scanner;
+
+public class Exercicio2601_02 {
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Vendas de Maçãs");
+        System.out.println("Maçã - R$ 0.30/unidade");
+        System.out.println("Promoção: Na compra acima de uma dúzia R$ 0.25/unidade");
+
+        System.out.print("Qual a quantidade de maçãs que deseja? ");
+        int quantidadeMacas = scan.nextInt();
+
+        double precoCompra = 0;
+
+        if (quantidadeMacas < 12) {
+            precoCompra = quantidadeMacas*0.30;
+        } else {
+            precoCompra = quantidadeMacas*0.25;
+        }
+
+        System.out.println("A quantidade de maçãs compradas é " + quantidadeMacas);
+        System.out.format("O valor a pagar pelas maçãs é %.2f", precoCompra);
+
+
+
+    }
+}
---
+++ src/br/com/walescko/aula01/Exercicio2601_03.java
@@ -0,0 +1,37 @@
+//Um programa que leia o salário bruto de um funcionário e mostre o valor liquido.
+//Se o salário for menor que R$1.100, o desconto de INSS é 6%
+//Até R$2.500, 8%
+//Até R$4.800 10%
+//Maior que R$ 4.800 15%
+package br.com.walescko.aula01;
+
+import java.util.Scanner;
+
+public class Exercicio26001_03 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Calculo do Salário Liquido");
+        System.out.print("Qual o valor do salário? ");
+        double salarioBruto = scan.nextDouble();
+
+        double salarioLiquido = 0;
+        double descontoINSS = 0;
+
+        if (salarioBruto <= 1100){
+            descontoINSS = salarioBruto*0.06;
+        }
+        if (salarioBruto <= 4800) {
+            descontoINSS = salarioBruto*0.10;
+        }
+        if (salarioBruto > 4800){
+            descontoINSS = 4800*0.15;
+        }
+
+        salarioLiquido = salarioBruto - descontoINSS;
+
+        System.out.format("O salário líquido de R$ %.2f é R$ %.2f, sendo o desconto do INSS igual %.2f", salarioBruto, salarioLiquido, descontoINSS);
+
+    }
+
+}
