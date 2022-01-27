b6cb5cd4124c0352106dcca258b826e5f48990c7
walescko <walescko@gmail.com>
2022-01-27 11:14:53 -0300

    java - exercicio 4 - aula 26/01/2022

---
+++ src/br/com/walescko/aula01/Exercicio2601_04.java
@@ -0,0 +1,39 @@
+//Escreva um algoritmo que calcule a média aritmética das 3 notas dos
+//alunos de uma classe. O algoritmo deverá ler, além das notas, o código
+//do aluno e deverá ser encerrado quando o código for igual a zero.
+
+package br.com.walescko.aula01;
+
+import java.util.Scanner;
+
+public class Exercicio2601_04 {
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Calculo de média de um aluno");
+        System.out.print("Digite o código do aluno: ");
+        int codigoAluno = scan.nextInt();
+
+        Double[] nota = new Double[3];
+
+        while(codigoAluno !=0) {
+            double soma = 0;
+            double media = 0;
+            for (int i = 0; i <= 2; i++) {
+                System.out.print("Digite a " + (i + 1) + "ª nota: ");
+                nota[i] = scan.nextDouble();
+            }
+
+            for (int i = 0; i <= 2; i++){
+                soma = soma + nota[i];
+            }
+
+            media = soma / nota.length;
+            System.out.format("A média do aluno é %.1f\n", media);
+
+            System.out.print("\nDigite o código no próximo aluno: ");
+            codigoAluno = scan.nextInt();
+        }
+    }
+}
