a36041e567419740de5006065158eb12163d5ee8
walescko <walescko@gmail.com>
2022-01-31 22:11:42 -0300

    classe model Pedido da Aula 2

--- src/br/com/walescko/aula02/model/Pedido.java
+++ src/br/com/walescko/aula02/model/Pedido.java
@@ -1,2 +1,66 @@
-package br.com.walescko.aula02.model;public class Pedido {
+package br.com.walescko.aula02.model;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class Pedido {
+
+    public String cliente;
+    public double valor;
+    public String[] itens = new String[5];
+
+    //Constructors
+    public Pedido(String cliente) {
+        this.cliente = cliente;
+    }
+
+    public Pedido(String cliente, double valor, String item1, String item2) {
+        this.cliente = cliente;
+        this.valor = valor;
+        this.itens[0] = item1;
+        this.itens[1] = item2;
+    }
+
+    public String mensagemFinal(){
+        String texto = "O Cliente " + cliente + " fez uma compra de " + valor + ", com os seguintes itens: "
+                + itens[0] + ", "
+                + itens[1] + ", "
+                + itens[2] + ", "
+                + itens[3] + ", "
+                + itens[4] + ".";
+        return texto;
+    }
+
+    public String retonaItens() {
+        String itensLista = "";
+        for (String item : this.itens) {
+            itensLista += item + ", ";
+        }
+        itensLista = itensLista.substring(0, itensLista.length() - 2);
+        return itensLista;
+    }
+
+    @Override
+    public String toString() {
+        return "O (A)  cliente='"
+                + cliente + '\'' +
+                " comprou os seguintes itens: " + retonaItens() +
+                ", totalizando R$ " + valor + ".";
+    }
+
+    public void receberItemValor(String item, double valor){
+        this.valor += valor;
+        for(int i = 0 ; i < this.itens.length; i++){
+            if(this.itens[i] == null){
+                this.itens[i] = item;
+                break;
+            }
+
+        }
+
+
+
+    }
+
+
 }
