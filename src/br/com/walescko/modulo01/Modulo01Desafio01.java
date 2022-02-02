3bfd69aaba4d9f5f71f8e12335629dab26430b00
walescko <walescko@gmail.com>
2022-02-02 14:22:47 -0300

    java - teste While

---
+++ src/br/com/walescko/modulo01/Modulo01Desafio01.java
@@ -0,0 +1,26 @@
+/**
+ * @author walescko
+ * since 02/01/2022
+ **/
+package br.com.walescko.modulo01;
+
+import java.util.Scanner;
+
+public class Modulo01Desafio01 {
+
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("TESTE DO WHILE");
+
+        System.out.println("Tecle '1' para continuar e '0' para encerrar: ");
+        int continue1 = scan.nextInt();
+
+        while (continue1 == 1){
+            System.out.println("Tecle '1' para continuar e '0' para encerrar: ");
+            continue1 = scan.nextInt();
+        }
+        System.out.println("FIM");
+    }
+}
---
+++ src/br/com/walescko/modulo01/Modulo01Desafio02.java
@@ -0,0 +1,2 @@
+package br.com.walescko.modulo01;public class Modulo01Desafio02 {
+}
