19b7572c5faa8cc6ff5155940d780588da5b950d
walescko <walescko@gmail.com>
2022-02-03 07:15:45 -0300

    java - desafio 1 concluido

--- src/br/com/walescko/modulo01/Modulo01Desafio01.java
+++ src/br/com/walescko/modulo01/Modulo01Desafio01.java
@@ -8,19 +8,58 @@ import java.util.Scanner;
 
 public class Modulo01Desafio01 {
 
-    public static void main(String[] args){
+    public static void ImcCalc(double height, double weight) {
 
+        double[] weightImc = {18.5, 24.9, 29.9, 39.9, 99999};
+        String[] imcClassification = {"Magreza", "Normal", "Sobrepeso", "Obesidade", "Obesidade Grave"};
+
+        double imc = weight/(height*height);
+
+        System.out.printf("O IMC é %.2f\n", imc);
+
+        int i = 0;
+        while (imc > weightImc[i]){
+            i++;
+        }
+        System.out.println("E a classificação é: " + imcClassification[i]);
+        Option();
+    }
+
+    public static void Option(){
         Scanner scan = new Scanner(System.in);
+        System.out.print("Tecle '1' para calcular o IMC ou '0' para encerrar:");
+        Integer option = scan.nextInt();
 
-        System.out.println("TESTE DO WHILE");
+        switch (option) {
+            case 1: {
+                System.out.println("Calculo do IMC");
+                System.out.print("Entre com a altura (m): ");
+                double height = scan.nextDouble();
+                System.out.print("Entre com o peso (kg): ");
+                double weigth = scan.nextDouble();
 
-        System.out.println("Tecle '1' para continuar e '0' para encerrar: ");
-        int continue1 = scan.nextInt();
+                ImcCalc(height, weigth);
 
-        do {
-            System.out.println("Tecle '1' para continuar e '0' para encerrar: ");
-            continue1 = scan.nextInt();
-        } while (continue1 == 1);
-        System.out.println("FIM");
+                break;
+            }
+            case 0: {
+                System.out.println("Você encerrou o programa!");
+                break;
+            }
+            default:
+                System.out.println("Opção Inválida!");
+                returnOption();
+        }
+    }
+
+//    Gambiarra para evitar erro - tenho que ver como resolver isso.
+    public static void returnOption(){
+        Option();
+    }
+
+    public static void main(String[] args) {
+
+        Option();
+
     }
 }
