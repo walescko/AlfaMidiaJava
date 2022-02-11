e01d10953f864e3ec430651f3be1d735a82e896f
walescko <walescko@gmail.com>
2022-02-11 18:32:30 -0300

    java - imc calc, simple exemple

--- src/br/com/walescko/modulo02/imcOO.java
+++ src/br/com/walescko/modulo02/imcOO.java
@@ -1,2 +1,18 @@
-package br.com.walescko.modulo02;public class imcOO {
+/**
+ * @author: walescko
+ * since 02/11/2022
+ * Exemplos da aula de Orientação a Objetos do Curso Desenvolvedor Java projeto +praTi
+ */
+package br.com.walescko.modulo02;
+
+public class imcOO {
+    public static void main(String[] args){
+
+        Double peso = 80.00;
+        Double altura = 1.80;
+
+        Double imc = (peso/(altura*altura));
+        System.out.println("Calculo do IMC");
+        System.out.println("O imc é " + imc);
+    }
 }
