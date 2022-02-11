437c22ed9f7728bf158525ad42f0a6255d6859be
walescko <walescko@gmail.com>
2022-02-11 19:21:11 -0300

    java - new object

--- src/br/com/walescko/modulo02/imc/Imc.java
+++ src/br/com/walescko/modulo02/imc/Imc.java
@@ -11,13 +11,23 @@ public class Imc {
     public double heigth;
     public double imc;
 
-    public Imc(double weigth, double heigth){
+    public Imc(double weigth, double heigth) {
         this.weigth = weigth;
         this.heigth = heigth;
     }
 
-    public void calcularImc(){
-        this.imc = ((this.weigth)/(this.heigth*this.heigth));
+    public void calcularImc() {
+        this.imc = ((this.weigth) / (this.heigth * this.heigth));
     }
 
+    public String classificacao() {
+        this.calcularImc();
+        String classificacao = "";
+        if (this.imc < 18.5) {classificacao = "magreza";}
+        if (this.imc < 24.9) {classificacao = "normal";}
+        if (this.imc < 29.9) {classificacao = "sobrepeso";}
+        if (this.imc < 39.9) {classificacao = "obesidade";}
+        if (this.imc >= 39.9) {classificacao = "obesidade grave";}
+        return classificacao;
+    }
 }
